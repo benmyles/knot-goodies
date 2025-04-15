a351a2248a2bbfd4a430e76470be715323721f57
Ben Myles <ben@2q31.com>
2025-04-15 12:59:18 -0700

    polish

--- serve_stale.lua
+++ serve_stale.lua
@@ -1,49 +1,38 @@
 -- MIT License
 --
--- always_serve_stale.lua is a knot resolver module that serves stale data when the TTL is expired,
--- similar to "optimistic caching" in AdGuard Home. It sets a short 10s TTL on the returned record
--- and kicks off an async resolve to fetch fresh data for future use.
+-- serve_stale.lua is an alternative version of the knot resolver serve_stale module that always serves
+-- stale data when the TTL is expired, similar to "optimistic caching" in AdGuard Home.
 --
--- Copy it into the modules dir (e.g. /usr/lib/x86_64-linux-gnu/knot-resolver/kres_modules/) then
--- add it to kresd.conf in the modules section:
+-- It sets a short 10s TTL on the returned record and kicks off an async resolve to fetch fresh data
+-- for future use.
 --
--- modules = {
---         'stats',
---         'always_serve_stale < cache',
--- }
+-- Install via:
+--   $ mv /usr/lib/knot-resolver/kres_modules/serve_stale.lua \
+--       /usr/lib/knot-resolver/kres_modules/serve_stale.lua.orig && \
+--     cp serve_stale.lua /usr/lib/knot-resolver/kres_modules/serve_stale.lua
+--
+-- And make sure you load the serve_stale module in kresd.conf.
 
 local M = {
-    -- served = 0,
-    -- skipped = 0,
 }
 
 local ffi = require('ffi')
 
-log_notice(ffi.C.LOG_GRP_SRVSTALE, '   => loading always_serve_stale module')
+log_debug(ffi.C.LOG_GRP_SRVSTALE, '   => loading always_serve_stale module')
 
 M.callback = ffi.cast("kr_stale_cb",
     function(ttl, name, type, qry)
         local n = kres.dname2str(qry.sname)
 
         if ttl + 3600 * 24 * 3 > 0 then -- at most 3 days stale
-            log_notice(ffi.C.LOG_GRP_SRVSTALE, '   => served stale data for ' .. n .. ' with TTL: ' .. tostring(ttl))
-
-            -- M.served = M.served + 1
-            -- if stats then
-            --     stats['astale.served'] = M.served
-            -- end
+            log_debug(ffi.C.LOG_GRP_SRVSTALE, '   => served stale data for ' .. n .. ' with TTL: ' .. tostring(ttl))
 
             resolve(n, qry.stype, qry.sclass, { 'NO_CACHE' }) -- fetch fresh data for future use
 
             return 10                                         -- short ttl for stale data
         else
-            log_notice(ffi.C.LOG_GRP_SRVSTALE,
+            log_debug(ffi.C.LOG_GRP_SRVSTALE,
                 '   => skipped serving stale data for ' .. n .. ' with old TTL: ' .. tostring(ttl))
-
-            M.skipped = M.skipped + 1
-            -- if stats then
-            --     stats['astale.skipped'] = M.skipped
-            -- end
 
             return -1
         end
