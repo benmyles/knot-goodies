32cc379274f0839fa902c4144e8690804d858f24
Ben Myles <ben@2q31.com>
2025-04-22 23:02:48 -0700

    logs

--- serve_stale.lua
+++ serve_stale.lua
@@ -1,9 +1,8 @@
 -- MIT License
 --
--- serve_stale.lua is an alternative version of the knot resolver serve_stale module that always serves
--- stale data when the TTL is expired, similar to "optimistic caching" in AdGuard Home.
+-- serve_stale.lua is an alternative version of the knot resolver serve_stale module that always serves stale data when the TTL is expired, similar to "optimistic caching" in AdGuard Home.
 --
--- It sets a short 10s TTL on the returned record and kicks off an async resolve to fetch fresh data
+-- It sets a short TTL on the response and kicks off an async resolve to fetch fresh data
 -- for future use.
 --
 -- Install via:
@@ -18,20 +17,20 @@ local M = {
 
 local ffi = require('ffi')
 
-log_debug(ffi.C.LOG_GRP_SRVSTALE, '   => loading always_serve_stale module')
+log_debug(ffi.C.LOG_GRP_SRVSTALE, '   => loading [optimistic] serve_stale module')
 
 M.callback = ffi.cast("kr_stale_cb",
     function(ttl, name, type, qry)
         local n = kres.dname2str(qry.sname)
 
-        if ttl + 3600 * 24 * 3 > 0 then -- at most 3 days stale
-            log_debug(ffi.C.LOG_GRP_SRVSTALE, '   => served stale data for ' .. n .. ' with TTL: ' .. tostring(ttl))
+        if ttl + 3600 * 24 > 0 then -- at most 1 day stale
+            log_notice(ffi.C.LOG_GRP_SRVSTALE, '   => served stale data for ' .. n .. ' with TTL: ' .. tostring(ttl))
 
             resolve(n, qry.stype, qry.sclass, { 'NO_CACHE' }) -- fetch fresh data for future use
 
             return 1
         else
-            log_debug(ffi.C.LOG_GRP_SRVSTALE,
+            log_notice(ffi.C.LOG_GRP_SRVSTALE,
                 '   => skipped serving stale data for ' .. n .. ' with old TTL: ' .. tostring(ttl))
 
             return -1
